c7ab49d49681ed11bfd1cee12b28304f6f5b763e
codekishore <kishorejain2002@gmail.com>
2024-06-06 19:06:30 +0530

    first commit

---
+++ fwms/admin/report-food-donor.php
@@ -0,0 +1,91 @@
+<?php
+session_start();
+error_reporting(0);
+include('includes/dbconnection.php');
+if (strlen($_SESSION['pgasaid']==0)) {
+ header('location:logout.php');
+  } else{
+   
+  ?>
+
+<!DOCTYPE html>
+<head>
+<title>Food Waste Management System|| Food Donor Reports  </title>
+
+<script type="application/x-javascript"> addEventListener("load", function() { setTimeout(hideURLbar, 0); }, false); function hideURLbar(){ window.scrollTo(0,1); } </script>
+<link rel="stylesheet" href="css/bootstrap.min.css" >
+<link href="css/style.css" rel='stylesheet' type='text/css' />
+<link href="css/style-responsive.css" rel="stylesheet"/>
+<link href='//fonts.googleapis.com/css?family=Roboto:400,100,100italic,300,300italic,400italic,500,500italic,700,700italic,900,900italic' rel='stylesheet' type='text/css'>
+<link rel="stylesheet" href="css/font.css" type="text/css"/>
+<link href="css/font-awesome.css" rel="stylesheet"> 
+<script src="js/jquery2.0.3.min.js"></script>
+<script src="http://js.nicedit.com/nicEdit-latest.js" type="text/javascript"></script>
+<script type="text/javascript">bkLib.onDomLoaded(nicEditors.allTextAreas);</script>
+</head>
+
+<body>
+<section id="container">
+<?php include_once('includes/header.php');?>
+<?php include_once('includes/sidebar.php');?>
+<section id="main-content">
+	<section class="wrapper">
+		<div class="form-w3layouts">          
+            <div class="row">
+                <div class="col-lg-12">
+                    <section class="panel">
+                        <header class="panel-heading">
+                            B/w Dates Report Date Selection
+                            <span class="tools pull-right">
+                                <a class="fa fa-chevron-down" href="javascript:;"></a>
+                                <a class="fa fa-cog" href="javascript:;"></a>
+                                <a class="fa fa-times" href="javascript:;"></a>
+                             </span>
+                        </header>
+                        <div class="panel-body">
+                            <div class="form">
+   
+                                <form class="cmxform form-horizontal " method="post" name="bwdatesreportds" action="bwdates-donor-report-details.php">
+                                   
+                                    <div class="form-group ">
+                                        <label for="adminname" class="control-label col-lg-3">From Dates</label>
+                                        <div class="col-lg-6">
+                                            <input class="form-control" id="fromdate" name="fromdate"  type="date" required="true">
+                                        </div>
+                                    </div>
+                                    <div class="form-group">
+                                        <label for="adminname" class="control-label col-lg-3">To Dates
+                                        </label>
+                                        <div class="col-lg-6">
+                                           <input class="form-control " id="todate" type="date" name="todate" required="true">
+                                        </div>
+                                    </div>
+                                  
+                                    <div class="form-group">
+                                        <div class="col-lg-offset-3 col-lg-6">
+                                          <p style="text-align: center;"> <button class="btn btn-primary" type="submit" name="submit">Update</button></p>
+                                           
+                                        </div>
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+
+                    </section>
+                </div>
+            </div>
+        </div>
+</section>
+		  <?php include_once('includes/footer.php');?>    
+</section>
+</section>
+<script src="js/bootstrap.js"></script>
+<script src="js/jquery.dcjqaccordion.2.7.js"></script>
+<script src="js/scripts.js"></script>
+<script src="js/jquery.slimscroll.js"></script>
+<script src="js/jquery.nicescroll.js"></script>
+<script src="js/jquery.scrollTo.js"></script>
+</body>
+</html>
+<?php }  ?>
